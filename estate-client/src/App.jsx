728cabf191b1519f1786355a42f415c3d2290683
tiraskar <sentribikram@gmail.com>
2023-11-12 21:10:53 +0545

    create header

--- estate-client/src/App.jsx
+++ estate-client/src/App.jsx
@@ -1,11 +1,13 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { About, Home, Login, Profile, Registration } from "./pages"
+import { Header } from "./components"
 
 export default function App() {
 
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/sign-in" element={<Login />} />
---
+++ estate-client/src/components/Header.jsx
@@ -0,0 +1,37 @@
+
+import { FaSearch } from "react-icons/fa"
+import { Link } from "react-router-dom"
+
+export default function Header() {
+
+    return (
+        <header className="bg-slate-200 shadow-md">
+            <div className="flex justify-between items-center mx-auto max-w-6xl p-3">
+                <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+                    <Link to='/'>
+                        <span className="text-slate-500">Tribikram</span>
+                        <span className="text-slate-700">Estate</span>
+                    </Link>
+                </h1>
+                <form className="bg-slate-100 p-2 rounded-lg flex items-center">
+                    <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
+                    <FaSearch className="text-slate-500" />
+                </form>
+                <ul className="flex gap-4 ">
+                    <Link to='/'>
+                        <li className="hidden sm:inline text-slate-700 hover:underline">
+                            Home
+                        </li>
+                    </Link>
+                    <Link to="/about">
+                        <li className=" hiddensm:inline text-slate-700 hover:underline">About</li>
+                    </Link>
+                    <Link to="/sign-in">
+                        <li className="sm:inline text-slate-700 hover:underline">Sign In</li>
+                    </Link>
+                </ul>
+            </div>
+        </header>
+    )
+
+}
